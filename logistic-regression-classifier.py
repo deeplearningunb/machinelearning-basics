3be1f0c6f90c6297b887e97e28c769e78f24bc20
João Pedro Sconetto <sconetto.joao@gmail.com>
2019-09-10 21:30:41 -0300

    Testing with Perceptron
    Changing the LogisticRegression to Perceptron
    
    Signed-off-by: João Pedro Sconetto <sconetto.joao@gmail.com>

--- logistic-regression-classifier.py
+++ logistic-regression-classifier.py
@@ -3,7 +3,7 @@
 # Importing the libraries
 from matplotlib.colors import ListedColormap
 from sklearn.metrics import confusion_matrix
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression, Perceptron
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 import numpy as np
@@ -11,7 +11,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Social_Network_Ads.csv')
+dataset = pd.read_csv("Social_Network_Ads.csv")
 x = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
@@ -26,7 +26,7 @@ x_train = sc.fit_transform(x_train)
 x_test = sc.transform(x_test)
 
 # Fitting classifier to the Training set
-classifier = LogisticRegression(random_state=0)
+classifier = Perceptron(random_state=0)
 classifier.fit(x_train, y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(x_test)
@@ -37,75 +37,63 @@ cm = confusion_matrix(y_test, y_pred)
 # Visualising the Training set results
 x_set, y_set = x_train, y_train
 X1, X2 = np.meshgrid(
-    np.arange(
-        start=x_set[:, 0].min() - 1,
-        stop=x_set[:, 0].max() + 1,
-        step=0.01
-    ),
-    np.arange(
-        start=x_set[:, 1].min() - 1,
-        stop=x_set[:, 1].max() + 1,
-        step=0.01
-    )
+    np.arange(start=x_set[:, 0].min() - 1, stop=x_set[:, 0].max() + 1, step=0.01),
+    np.arange(start=x_set[:, 1].min() - 1, stop=x_set[:, 1].max() + 1, step=0.01),
 )
 
 plt.contourf(
     X1,
     X2,
-    classifier.predict(
-        np.array([X1.ravel(), X2.ravel()]).T
-    ).reshape(X1.shape),
+    classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
     alpha=0.75,
-    cmap=ListedColormap(('pink', 'lightgreen'))
+    cmap=ListedColormap(("pink", "lightgreen")),
 )
 
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 
 for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
-                c=ListedColormap(('red', 'green'))(i), label=j)
+    plt.scatter(
+        x_set[y_set == j, 0],
+        x_set[y_set == j, 1],
+        c=ListedColormap(("red", "green"))(i),
+        label=j,
+    )
 
-plt.title('Classifier (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
+plt.title("Classifier (Training set) [Perceptron]")
+plt.xlabel("Age")
+plt.ylabel("Estimated Salary")
 plt.legend()
 plt.show()
 
 # Visualising the Test set results
 x_set, y_set = x_test, y_test
 X1, X2 = np.meshgrid(
-    np.arange(
-        start=x_set[:, 0].min() - 1,
-        stop=x_set[:, 0].max() + 1,
-        step=0.01
-    ),
-    np.arange(
-        start=x_set[:, 1].min() - 1,
-        stop=x_set[:, 1].max() + 1,
-        step=0.01
-    )
+    np.arange(start=x_set[:, 0].min() - 1, stop=x_set[:, 0].max() + 1, step=0.01),
+    np.arange(start=x_set[:, 1].min() - 1, stop=x_set[:, 1].max() + 1, step=0.01),
 )
 
 plt.contourf(
     X1,
     X2,
-    classifier.predict(
-        np.array([X1.ravel(), X2.ravel()]).T
-    ).reshape(X1.shape),
+    classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
     alpha=0.75,
-    cmap=ListedColormap(('pink', 'lightgreen'))
+    cmap=ListedColormap(("pink", "lightgreen")),
 )
 
 plt.xlim(X1.min(), X1.max())
 plt.ylim(X2.min(), X2.max())
 
 for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(x_set[y_set == j, 0], x_set[y_set == j, 1],
-                c=ListedColormap(('red', 'green'))(i), label=j)
+    plt.scatter(
+        x_set[y_set == j, 0],
+        x_set[y_set == j, 1],
+        c=ListedColormap(("red", "green"))(i),
+        label=j,
+    )
 
-plt.title('Classifier (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
+plt.title("Classifier (Test set) [Perceptron]")
+plt.xlabel("Age")
+plt.ylabel("Estimated Salary")
 plt.legend()
 plt.show()
