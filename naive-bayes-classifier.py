801ece726a79688d5bc3c97ff1054ff200e7f003
Gustavo-Nogueira <gustavonr.13@gmail.com>
2019-11-30 19:09:10 -0300

    Adição do classificador Naive Bayes.

--- naive-bayes-classifier.py
+++ naive-bayes-classifier.py
@@ -1,17 +1,15 @@
-# Classification template
-
-# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
+# Dataset
 dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -21,10 +19,14 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Fitting classifier to the Training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
+
+from sklearn.naive_bayes import GaussianNB
+
+classifier = GaussianNB()
 classifier.fit(X_train,y_train)
+
 # Predicting the Test set results
+
 y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
