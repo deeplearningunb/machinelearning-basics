ab70852f67a837ff5fdb0fe4a50f71189ccf0f70
Nilo Mendonca <nilojrmendonca@gmail.com>
2020-09-05 18:52:10 -0300

    Alteracoes no classificador

--- logistic-regression-classifier.py
+++ logistic-regression-classifier.py
@@ -11,7 +11,7 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -20,6 +20,27 @@ sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
+# Visualising the set results
+def plotarGrafico(X_dados, y_dados, classifier, tipo_classificacao, nome):
+	from matplotlib.colors import ListedColormap
+	X_set, y_set = X_dados, y_dados
+	X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+	                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+	plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+	             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
+	plt.xlim(X1.min(), X1.max())
+	plt.ylim(X2.min(), X2.max())
+	for i, j in enumerate(np.unique(y_set)):
+	    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+	                c = ListedColormap(('red', 'green'))(i), label = j)
+	plt.title('Classifier '+nome+' ('+tipo_classificacao+' set)')
+	plt.xlabel('Age')
+	plt.ylabel('Estimated Salary')
+	plt.legend()
+	plt.show()
+
+#############################################
+### Regressão Logistica
 # Fitting classifier to the Training set
 from sklearn.linear_model import LogisticRegression
 classifier = LogisticRegression(random_state = 0)
@@ -31,38 +52,40 @@ y_pred = classifier.predict(X_test)
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test, y_pred)
 
-# Visualising the Training set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_train, y_train
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Training set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
+# Plota os graficos
+plotarGrafico(X_train, y_train, classifier, 'Training', 'Logistic Regression')
+plotarGrafico(X_test, y_test, classifier, 'Test', 'Logistic Regression')
 
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = X_test, y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
-                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
-plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('pink', 'lightgreen')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
-                c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Classifier (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
+#############################################
+### Regressão ARD bayesiana
+# Fitting classifier to the Training set
+from sklearn import linear_model
+classifier = linear_model.ARDRegression()
+classifier.fit(X_train,y_train)
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cmARD = confusion_matrix(y_test, y_pred.round())
+
+# Plota os graficos
+plotarGrafico(X_train, y_train, classifier, 'Training', 'ARD Regression')
+plotarGrafico(X_test, y_test, classifier, 'Test', 'ARD Regression')
+
+#############################################
+### Regressão ARD bayesiana
+# Fitting classifier to the Training set
+from sklearn import linear_model
+classifier = linear_model.LinearRegression()
+classifier.fit(X_train,y_train)
+# Predicting the Test set results
+y_pred = classifier.predict(X_test)
+
+# Making the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cmLinear = confusion_matrix(y_test, y_pred.round())
+
+# Plota os graficos
+plotarGrafico(X_train, y_train, classifier, 'Training', 'Linear Regression')
+plotarGrafico(X_test, y_test, classifier, 'Test', 'Linear Regression')
