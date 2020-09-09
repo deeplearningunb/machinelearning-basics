daf614b694d7709b1a7f06cf17be69533de15060
Pedro Helias <30875663+pedrohelias@users.noreply.github.com>
2020-09-09 18:33:22 -0300

    Add novos classificadores

--- logistic-regression-classifier.py
+++ logistic-regression-classifier.py
@@ -78,7 +78,7 @@ classifier = Perceptron(random_state = 0)
 classifier.fit(X_train,y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
-
+ 
 
 # Making the Confusion Matrix
 from sklearn.metrics import confusion_matrix
