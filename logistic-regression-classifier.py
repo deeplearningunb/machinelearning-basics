0a861627e20a8cf4f616cd79f51a7fa690926d1b
Guilherme Siqueira <guilhersiqueira@gmail.com>
2020-09-11 23:45:25 -0300

    changes classifier to Perceptron

--- logistic-regression-classifier.py
+++ logistic-regression-classifier.py
@@ -11,7 +11,7 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -21,8 +21,8 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Fitting classifier to the Training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
+from sklearn.linear_model import Perceptron
+classifier = Perceptron(random_state = 0)
 classifier.fit(X_train,y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
