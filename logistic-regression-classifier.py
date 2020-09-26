49f264e03756218be1fe04cf9aa1ab9604d71180
Fernando Aguilar <fernando.aguilar@hotmail.com.br>
2020-09-26 10:21:55 -0300

    Refat: change classifier for K nearest neighbors

--- logistic-regression-classifier.py
+++ logistic-regression-classifier.py
@@ -21,8 +21,9 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # Fitting classifier to the Training set
-from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.pipeline import make_pipeline
+classifier = KNeighborsClassifier(n_neighbors = 6)
 classifier.fit(X_train,y_train)
 # Predicting the Test set results
 y_pred = classifier.predict(X_test)
