1458ec40b7ddf92cbe6d42132eeda3f0aba83079
Arthur <arthurarp@hotmail.com>
2021-03-05 23:48:05 -0300

    Mudando o classificador de Regressão Logística para KNN

--- logistic-regression-classifier.py
+++ logistic-regression-classifier.py
@@ -11,7 +11,7 @@ X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
 # Splitting the dataset into the Training set and Test set
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
 
 # Feature Scaling
@@ -22,14 +22,19 @@ X_test = sc.transform(X_test)
 
 # Fitting classifier to the Training set
 from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression(random_state = 0)
+from sklearn.neighbors import KNeighborsClassifier
+classifier = KNeighborsClassifier()
 classifier.fit(X_train,y_train)
 # Predicting the Test set results
-y_pred = classifier.predict(X_test)
+knn_y_pred = classifier.predict(X_test)
 
 # Making the Confusion Matrix
-from sklearn.metrics import confusion_matrix
-cm = confusion_matrix(y_test, y_pred)
+from sklearn.metrics import confusion_matrix, classification_report
+cm = confusion_matrix(y_test, knn_y_pred)
+
+# printando a acurácia do KNN
+
+print('accuracy of knn = {}'.format(classification_report(y_test, knn_y_pred)))
 
 # Visualising the Training set results
 from matplotlib.colors import ListedColormap
